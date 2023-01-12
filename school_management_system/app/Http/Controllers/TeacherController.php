07ed2ecd22234fce871afb8a2a7d0c770739811c
mohammadali63 <taheralinur@gmail.com>
2023-01-12 23:59:32 +0600

    Teacher Create & Data submit database

--- school_management_system/app/Http/Controllers/TeacherController.php
+++ school_management_system/app/Http/Controllers/TeacherController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Teacher;
 use Illuminate\Http\Request;
 
 class TeacherController extends Controller
 {
+    private $code,$updateString,$id,$name,$year,$teacher,$teachers;
     public function index()
     {
         return view('admin.teacher.add');
@@ -13,7 +15,21 @@ class TeacherController extends Controller
 
     public function CreateTeacher(Request $request)
     {
-        return $request->all();
+        $this->teacher = Teacher::orderBy('id', 'desc')->first();
+        if ($this->teacher)
+        {
+            $this->id = $this->teacher->id + 1;
+        }
+        else
+        {
+            $this->id = 1;
+        }
+        $this->updateString = preg_replace('/[^A-Za-z0-9]/','',$request->name);
+        $this->name = strtoupper(substr($this->updateString, 0,3));
+        $this->year = date('Y');
+        $this->code = $this->name.$this->year.$this->id;
+        Teacher::newTeacher($request, $this->code);
+        return redirect('/add/teacher')->with('massage','Teacher Info Save Successfully');
     }
 
     public function manageTeacher()
--- school_management_system/app/Models/Teacher.php
+++ school_management_system/app/Models/Teacher.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use GuzzleHttp\Psr7\Request;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -9,15 +10,25 @@ class Teacher extends Model
 {
     use HasFactory;
     private static $teacher;
-    public static function newTeacher($request)
+    private static $image,$imageName,$imageUrl,$directory;
+    public static function  getImageUrl($request)
+    {
+        self::$image = $request->file('image');
+        self::$imageName = self::$image->getClientOriginalName();
+        self::$directory = 'teacher-images/';
+        self::$image->move(self::$directory,self::$imageName);
+        self::$imageUrl =self::$directory.self::$imageName;
+        return self::$imageUrl;
+    }
+    public static function newTeacher($request,$code)
     {
         self::$teacher = new Teacher();
         self::$teacher->name = $request->name;
-        self::$teacher->code = '';
+        self::$teacher->code = $code;
         self::$teacher->email = $request->email;
         self::$teacher->mobile = $request->mobile;
         self::$teacher->address = $request->address;
-        self::$teacher->image = '';
+        self::$teacher->image = self::getImageUrl($request);
         self::$teacher->save();
     }
 }
